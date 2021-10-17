33ad5a185c3153ea939f3c41cfcd8969f20cdff7
Marcello Sylvester Bauer <sylv@sylv.io>
2021-10-18 01:38:03 +0200

    fix function posix shell
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 bashrc_src="[ -f .bashrc.local ] && source .bashrc.local"
 
-function dotconfig() {
+dotconfig() {
 	local config="${dir}/dots/$1"
 	local dest="${HOME}/$2"
 	mkdir -p $(dirname $dest)
@@ -17,7 +17,7 @@ function dotconfig() {
 	ln -sf "$config" "$dest"
 }
 
-function bashrc_hook() {
+bashrc_hook() {
   if ! grep -Fxq "$bashrc_src" "${HOME}/.bashrc"; then
     echo "append \"$bashrc_src\" to .bashrc"
     echo "$bashrc_src" >> ${HOME}/.bashrc
