66a0eaa26f355cb7729f47b17c561290abf03fd5
Marcello Sylvester Bauer <sylv@sylv.io>
2021-10-17 23:52:57 +0200

    Add setup.sh
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+function dotconfig() {
+	local config="${dir}/dots/$1"
+	local dest="${HOME}/$2"
+	mkdir -p $(dirname $dest)
+	if [ -e "$dest" -a ! -L "$dest" ]; then
+		>&2 echo "moving old config to ${dest}.old"
+		mv "${dest}"{,.old}
+	fi
+	echo "linking $config -> $dest"
+	ln -sf "$config" "$dest"
+}
+
+dotconfig init.vim .config/nvim/init.vim
+dotconfig tmux.conf .tmux.conf
