d4aebbdf7ef5ea29fef411cd639becb02661da4e
Marcello Sylvester Bauer <sylv@sylv.io>
2021-10-20 18:43:11 +0200

    remove curly braces
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- setup.sh
+++ setup.sh
@@ -6,21 +6,21 @@ dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
 bashrc_src="[ -f ~/.bashrc.local ] && source ~/.bashrc.local"
 
 dotconfig() {
-  config="${dir}/dots/$1"
-  dest="${HOME}/$2"
+  config="$dir/dots/$1"
+  dest="$HOME/$2"
   mkdir -p "$(dirname "$dest")"
   if [ -e "$dest" ] && [ ! -L "$dest" ]; then
     >&2 echo "moving old config to ${dest}.old"
-    mv "${dest}" "${dest}.old"
+    mv "$dest" "$dest.old"
   fi
   echo "linking $config -> $dest"
   ln -sf "$config" "$dest"
 }
 
 bashrc_hook() {
-  if ! grep -Fxq "$bashrc_src" "${HOME}/.bashrc"; then
+  if ! grep -Fxq "$bashrc_src" "$HOME/.bashrc"; then
     echo "append \"$bashrc_src\" to .bashrc"
-    echo "$bashrc_src" >> "${HOME}/.bashrc"
+    echo "$bashrc_src" >> "$HOME/.bashrc"
   fi
 }
 
