306224bb4be7d2eb03ea16b18bea1cc3eef5bc1e
Marcello Sylvester Bauer <sylv@sylv.io>
2021-10-21 08:31:28 +0200

    Add setup remove
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- setup.sh
+++ setup.sh
@@ -4,31 +4,64 @@ set -e
 
 dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
 bin="$HOME/.local/bin"
-bashrc_src="[ -f ~/.bashrc.local ] && source ~/.bashrc.local"
 
-nvim_src="https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
-nvim="$bin/nvim"
+bashrc="$dir/dots/bashrc"
+bash_hook="[ -f $bashrc ] && source $bashrc"
 
-dotconfig() {
+# default
+arg="${1:-install}"
+
+case $arg in
+  install)
+    op="add"
+    ;;
+  remove)
+    op="del"
+    ;;
+  *)
+    echo "usage: $0 <install/remove>" 1>&2
+    exit 1
+    ;;
+esac
+
+add_dotconfig() {
   config="$dir/dots/$1"
   dest="$HOME/$2"
   mkdir -p "$(dirname "$dest")"
   if [ -e "$dest" ] && [ ! -L "$dest" ]; then
-    >&2 echo "moving old config to ${dest}.old"
+    echo "move: old $dest -> $dest.old"
     mv "$dest" "$dest.old"
   fi
-  echo "link $config -> $dest"
+  echo "link: $dest -> $config"
   ln -sf "$config" "$dest"
 }
 
-bashrc_hook() {
-  if ! grep -Fxq "$bashrc_src" "$HOME/.bashrc"; then
-    echo "append \"$bashrc_src\" to .bashrc"
-    echo "$bashrc_src" >> "$HOME/.bashrc"
+del_dotconfig() {
+  dest="$HOME/$2"
+  if [ -L "$dest" ]; then
+    echo "unlink: $dest"
+    rm "$dest"
+    if [ -e "$dest.old" ]; then
+       echo "move: $dest.old -> $dest"
+       mv "$dest.old" "$dest"
+    fi
   fi
 }
 
+dotconfig() {
+  case $op in
+    add)
+      add_dotconfig "$1" "$2"
+      ;;
+    del)
+      del_dotconfig "$1" "$2"
+      ;;
+  esac
+}
+
 install_nvim() {
+  nvim_src="https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
+  nvim="$bin/nvim"
   if [ ! -x "$nvim" ]; then
     echo "get $nvim"
     mkdir -p "$bin"
@@ -39,10 +72,32 @@ install_nvim() {
   fi
 }
 
+add_bashrc_hook() {
+  if ! grep -Fxq "$bash_hook" "$HOME/.bashrc"; then
+    echo "append: \"$bash_hook\" to .bashrc"
+    echo "$bash_hook" >> "$HOME/.bashrc"
+  fi
+}
+
+del_bashrc_hook() {
+  if grep -Fxq "$bash_hook" "$HOME/.bashrc"; then
+    echo "remove: \"$bash_hook\" from .bashrc"
+    echo "WIP: not implemented" 1>&2
+  fi
+}
+
+bashrc_hook() {
+  case $op in
+    add)
+        add_bashrc_hook
+      ;;
+    del)
+        del_bashrc_hook
+      ;;
+  esac
+}
+
 dotconfig init.vim .config/nvim/init.vim
 dotconfig tmux.conf .tmux.conf
-dotconfig bashrc .bashrc.local
-
 install_nvim
-
 bashrc_hook
