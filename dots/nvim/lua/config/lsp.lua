730b282231b433a9f6d685fa0b3d93dc341f22e5
Marcello Sylvester Bauer <sylv@sylv.io>
2022-05-11 18:23:08 +0200

    nvim: lsp: use Tab to complete
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- dots/nvim/lua/config/lsp.lua
+++ dots/nvim/lua/config/lsp.lua
@@ -137,19 +137,10 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<Tab>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
